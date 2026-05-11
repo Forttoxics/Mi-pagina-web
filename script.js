3bced98ba19c0195c4e234ef4ce352cf1642bf47
Forttoxics <fforttoxics@gmail.com>
2026-05-11 04:52:11 -0600

    Update script.js

--- script.js
+++ script.js
@@ -191,7 +191,7 @@
       texto: 'Vas donde otros dudan, porque tu alma florece cuando descubre.'
     },
     {
-      icono: '/Mi-pagina-web/assets/iconoa/naturaleza/sword.svg',
+      icono: '/Mi-pagina-web/assets/iconos/naturaleza/sword.svg',
       nombre: 'Guerrero',
       texto: 'Luchas por lo que quieres, pero nunca olvides que también mereces tu propia paz.'
     },
